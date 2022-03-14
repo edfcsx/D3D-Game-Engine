dd9a8739cc7d86a7df09773412db156be8e3e559
Silva, Eduardo Cipriano <edfcsx@gmail.com>
2022-03-13 23:41:58 -0300

    Created Pixel and Vertex shaders

---
+++ D3D-Game-Engine/Vertex.hlsl
@@ -0,0 +1,46 @@
+/**********************************************************************************
+// Vertex (Arquivo de Sombreamento)
+//
+// Criação:     11 Jul 2007
+// Atualização: 13 Ago 2021
+// Compilador:  D3DCompiler
+//
+// Descrição:   Define um vertex shader que apenas multiplica os vértices
+//              por uma matriz de transformação e projeção
+//
+**********************************************************************************/
+
+// matriz de transformação e projeção
+cbuffer ConstantBuffer {
+	float4x4 WolrdViewProj;
+};
+
+// estrutura dos vertices de entrada
+struct VertexIn {
+	float3 Pos	 : POSITION;
+	float4 Color : COLOR;
+	float2 Tex	 : TEXCOORD;
+};
+
+// estrutura dos vertices de saída
+struct VertexOut {
+	float4 Pos: SV_POSITION;
+	float4 Color: COLOR;
+	float2 Tex: TEXCOORD;
+};
+
+// programa principal do vertex shader
+VertexOut main(VertexIn vIn) {
+	VertexOut vOut;
+
+	// transforma os vertices para coordenadas na tela
+	vOut.pos = mul(float4(vIn.Pos, 1.0f), WorldViewProj);
+
+	// mantém as cores inalteradas
+	vOut.Color = vIn.Color;
+
+	// mantém as coordenadas da textura inalteradas
+	vOut.Tex = vIn.Tex;
+
+	return vOut;
+}
